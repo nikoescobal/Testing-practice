6cf3dc724357292f8384be31d6108020ad734981
Niko Escobal <niko.escobal@gmail.com>
2021-09-30 16:52:01 +0800

    Add calculator operations for sum, difference, product, and quotient functions.

--- functions/script.js
+++ functions/script.js
@@ -4,9 +4,21 @@ const stringLength = (str) => {
   }
 };
 
-const reverseString = (str) => str.split("").reverse().join("");
+const reverseString = (str) => str.split('').reverse().join('');
 
+
+class Calculator {
+  static sum = (num1, num2) => (num1 * 10 + num2 * 10) / 10;
+
+  static difference = (num1, num2) => (num1 * 10 - num2 * 10) / 10;
+
+  static product = (num1, num2) => num1 * num2;
+
+  static quotient = (num1, num2) => num1 / num2;
+
+}
 module.exports = {
   stringLength,
   reverseString,
+  Calculator
 };
--- test.js
+++ test.js
@@ -3,22 +3,39 @@ const {
 } = require('@jest/globals');
 const {
   stringLength,
-  reverseString
+  reverseString,
+  Calculator,
+  capitalizeFirstChar,
 } = require('./functions/script.js');
 
 describe('Testing Practice', () => {
   describe('Task 1: String length function', () => {
     test('Should return length of a sting', () => {
       expect(stringLength('hello')).toBe(5);
+      expect(stringLength('hello')).not.toBe(4);
       expect(stringLength('hello')).toBeGreaterThanOrEqual(1);
-      expect(stringLength('hello')).toBeLessThanOrEqual(10);
+      expect(stringLength('hello')).not.toBeGreaterThanOrEqual(20);
+      expect(stringLength('hello')).not.toBeLessThanOrEqual(0);
+      expect(stringLength('hello')).not.toBeUndefined();
+      expect(stringLength('hello')).not.toBeNull();
     });
   });
 
   describe('Task 2: Reverse a string function', () => {
     test('Should return the reverse of a string', () => {
       expect(reverseString('hello')).toBe('olleh');
-      expect(reverseString('hello')).toEqual(expect.not.stringContaining('hello'));
+      expect(reverseString('hello')).toEqual(
+        expect.not.stringContaining('hello')
+      );
+    });
+  });
+
+  describe('Task 3: Testing Calculation Operation', () => {
+    test('Should return sum of numbers', () => {
+      expect(Calculator.sum(4, 3)).toEqual(7);
+      expect(Calculator.difference(10, 3)).toEqual(7);
+      expect(Calculator.product(4, 3)).toEqual(12);
+      expect(Calculator.quotient(20, 4)).toEqual(5);
     });
   });
 });
